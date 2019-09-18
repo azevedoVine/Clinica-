92aeb3dcf0bf5f354d0a308107bec50a08a6349c
azevedoVine <vine1957@hotmail.com>
2019-09-18 06:41:05 -0300

    Teste

---
+++ Admin/login.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+            <title>Clínica ABC - Login</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+            <link rel="stylesheet" type="text/css" href="assets/css/all.css">
+            <link rel="icon" type="image/png" sizes="32x32" href="assets/img/icone-browser.png">
+
+        </head>
+
+    <style>
+        
+    </style>
+<body>
+
+    <div id="logo-login">
+		<img src="assets/img/logo.png" width="80" height="80">         
+	</div>
+
+</body>
+</html>
